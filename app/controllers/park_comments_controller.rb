c2b233c35b4ff05df948a6b0404df17eb9268820
Your Name <you@example.com>
2024-06-28 23:39:36 +0000

    comments_count

--- app/controllers/park_comments_controller.rb
+++ app/controllers/park_comments_controller.rb
@@ -1,10 +1,8 @@
 class ParkCommentsController < ApplicationController
   def create
-    #byebug
     park = Park.find(params[:park_id])
     comment = current_user.park_comments.new(park_comment_params)
     comment.park_id = park.id
-    #byebug
     comment.save
     redirect_to park_path(park)
   end
